a7c37844d04a2122e1d147215bbd27c002dcf6eb
Kayque <kayquecarvalho020@gmail.com>
2025-08-11 11:23:53 -0300

    Refactor: Adcionando estrutura de laço

--- GameV1/src/br/com/fiap2espa/GameV1.java
+++ GameV1/src/br/com/fiap2espa/GameV1.java
@@ -1,6 +1,7 @@
 package br.com.fiap2espa; //endereço do pacote onde se encontra a nossa class
 
 import java.util.Scanner;
+//JAVA a linguagem compilada e especialista em desing de software
 
 public class GameV1 {
     public static void main(String[] args) {
@@ -10,21 +11,29 @@ public class GameV1 {
         System.out.println("Qual é o seu nome?");
         Scanner leitor = new Scanner(System.in);
         String nome = leitor.next();
-        leitor.nextLine();
+        leitor.nextLine(); //Limpa o buffer apenas para consumir o primeiro nome
+
+        int tentativas = 3;
 
         System.out.println("\nOlá " + nome + " você é capaz de descobrir o número sorteado?");
+        System.out.println("Você terá " + tentativas+ " tentativas");
 
         int numeroSorteado = 47;
 
-        System.out.println("Digite um número entre 0 e 101");
-        int numeroEscolhido = leitor.nextInt();
-        //System.out.println("Você digitou o número " +  leitor.nextInt());
+        while(tentativas > 0) {
+            System.out.println("Digite um número entre 0 e 101");
+            int numeroEscolhido = leitor.nextInt();
+            //System.out.println("Você digitou o número " +  leitor.nextInt());
 
-        if(numeroEscolhido == numeroSorteado) {
-            System.out.println("Parabéns " + nome + ", você acertou o número!!!");
-        } else {
-            System.out.println("Você perdeu :(");
+            if(numeroEscolhido == numeroSorteado) {
+                System.out.println("Parabéns " + nome + ", você acertou o número!!!");
+                tentativas = 0;
+            } else {
+                System.out.println("Você perdeu :(");
+            }
+            tentativas --;
+            //tentativas -= tentativas;
+            //tentativas = tentativas - 1;
         }
-
     }
 }
