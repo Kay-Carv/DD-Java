5c805df68204fd10535a014f327ec491869569e7
Kayque <kayquecarvalho020@gmail.com>
2025-08-18 11:34:23 -0300

    Chore: Arumar depois

---
+++ 02-Atividade_Metodos/src/br/com/fiap2ESPA/NotasV2.java
@@ -0,0 +1,40 @@
+package br.com.fiap2ESPA;
+
+import java.util.Scanner;
+
+public class NotasV2 {
+    public  static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+
+
+    }
+}
+//        Scanner leitor = new Scanner(System.in);
+//
+//        //Variável da primeira nota
+//        int qtdNotas = 1;
+//
+//        System.out.println("=== Sistema de Notas ===");
+//        String nome = coletarNome(leitor);
+//        double calcularMedia(nome, qtdNotas);
+//        }
+//
+//    // Método de coleta do nome
+//    static String coletarNome(Scanner leitor) {
+//        System.out.println("Digite o nome do aluno");
+//        String nome = leitor.next();
+//        leitor.nextLine();
+//        return nome;
+//    }
+//
+//    //Método para calcular a média
+//    static void calcularMedia(nome notas) {
+//        while (notas <= 3) {
+//            System.out.println("Digite a " + notas + "° nota do aluno" + nome );
+//        }
+//        notas --;
+//    }
+//
+//    //Resultado aprovado
+//}
