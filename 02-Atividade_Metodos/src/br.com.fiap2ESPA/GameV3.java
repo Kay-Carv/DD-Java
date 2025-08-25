7dd40922df2f5bc930b4ccc107606e25e644da5b
Kayque <kayquecarvalho020@gmail.com>
2025-08-25 10:31:02 -0300

    Feat: códigos do gameV3 e jogo da última aula

---
+++ 02-Atividade_Metodos/src/br.com.fiap2ESPA/GameV3.java
@@ -0,0 +1,24 @@
+package br.com.fiap2ESPA;
+
+import java.util.Scanner;
+
+//Classe Driver
+public class GameV3 {
+    public static void main(String[] args) {
+        //Configuração do jogo:
+        Scanner leitor = new Scanner(System.in);
+
+        //Jogo jogo1 = new Jogo(59, 3);
+
+        //Execução do jogo:
+
+
+        /*(jogo1.apresentarSaudação();
+        jogo1.jogador = jogo1.coletarNome(leitor);
+        jogo1.apresentarDesafio();
+        jogo1.jogar(leitor);
+        jogo1.finalizarJogo(); */
+
+        leitor.close();
+    }//Fim do método main
+}//Fim da classe
---
+++ 02-Atividade_Metodos/src/br.com.fiap2ESPA/Jogo.java
@@ -0,0 +1,65 @@
+package br.com.fiap2ESPA;
+
+import java.util.Scanner;
+
+public class Jogo {//Classe de Objeto
+    //Propriedades ou Atributos:
+    int númeroSorteado;
+    int tentativas;
+    String jogador;
+
+    //Construtores:
+    Jogo() {}
+
+    Jogo(int nS, int t) {
+        númeroSorteado = nS;
+        tentativas = t;
+    }
+
+    //Comportamentos:
+    void chamarDesafiador(Scanner l) {
+        System.out.println("Espaço para o desafiador. O jogador não deve observar!");
+        System.out.println("Desafiador, defina o número sorteado:");
+        númeroSorteado = l.nextInt();
+        System.out.println("Desafiador, defina o número de tentativas:");
+        tentativas = l.nextInt();
+        System.out.println("Desafiador, chame o jogador!");
+    }
+
+    static void apresentarSaudação() {
+        System.out.println("Bem vindo ao jogo da turma 2ESPA!");
+        System.out.println("Nesse jogo, você escolherá um número entre 1 e 100!");
+    }
+
+    String coletarNome(Scanner l) {
+        System.out.println("Qual é o seu nome?");
+        String nome = l.next();
+        l.nextLine();
+        System.out.println("Olá, " + nome + "!");
+        return nome;
+    }
+
+    void apresentarDesafio() {
+        System.out.println("Você é capaz de descobrir o número sorteado?");
+        System.out.println("Você terá " + tentativas + " tentativas");
+    }
+
+    void jogar(Scanner l) {
+        while (tentativas > 0) {
+            System.out.println("Digite um número entre 1 e 100:");
+            int númeroEscolhido = l.nextInt();
+
+            if(númeroEscolhido == númeroSorteado) {
+                System.out.println("Parabéns, " + jogador + ", você ganhou!");
+                break;
+            } else {
+                System.out.println("Que pena, " + jogador + ", você perdeu!");
+            }
+            tentativas--;
+        }
+    }
+
+    void finalizarJogo() {
+        System.out.println("Fim do Jogo!");
+    }
+}
